9bb34906ad99bfaa324c6248c658a7f384a2502a
jordy repi <joju19grifith@gmail.com>
2024-02-14 20:33:33 +0700

    fix: add package in package.json

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,12 +1,15 @@
 import Image from 'next/image';
 import styles from './page.module.css';
 import { Button } from 'flowbite-react';
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <>
-      <div className="text-[200px] text-red-700"> heloo</div>
-      <Button>testtt</Button>
+      <div className="text-[200px] text-red-700">Landingggg</div>
+      <Link href={'/login'}>
+        <text>testtt</text>
+      </Link>
     </>
   );
 }
