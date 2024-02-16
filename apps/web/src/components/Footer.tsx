29c44d6004b1fc188de969ec53489fa93fc456d2
palingtegar <tegar.itmamul@gmail.com>
2024-02-16 16:47:16 +0700

    feat: create footer

--- apps/web/src/components/Footer.tsx
+++ apps/web/src/components/Footer.tsx
@@ -1,3 +1,28 @@
-export const Footer = () => {
-  return <div>Footer</div>;
+import {
+  Footer,
+  FooterBrand,
+  FooterCopyright,
+  FooterDivider,
+  FooterLink,
+  FooterLinkGroup,
+} from 'flowbite-react';
+
+export const FooterComp = () => {
+  return (
+    <Footer container>
+      <div className="w-full text-center max-w-7xl mx-auto">
+        <div className="w-full justify-between sm:flex sm:items-center sm:justify-between">
+          <FooterBrand href="#" src="/LOGOBRDL.png" name="BORDL" />
+          <FooterLinkGroup>
+            <FooterLink href="#">About</FooterLink>
+            <FooterLink href="#">Privacy Policy</FooterLink>
+            <FooterLink href="#">Licensing</FooterLink>
+            <FooterLink href="#">Contact</FooterLink>
+          </FooterLinkGroup>
+        </div>
+        <FooterDivider />
+        <FooterCopyright href="#" by="BORDL™" year={2024} />
+      </div>
+    </Footer>
+  );
 };
