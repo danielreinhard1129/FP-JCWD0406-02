e8016bf0b60cc45e96efe83f1bf698109a18d2e0
palingtegar <148305220+palingtegar@users.noreply.github.com>
2024-02-16 16:37:24 +0700

    feat: create hero section (#6)

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -2,7 +2,12 @@ import Image from 'next/image';
 import styles from './page.module.css';
 import { Button } from 'flowbite-react';
 import Link from 'next/link';
+import { Banner } from '@/components/Banner';
 
 export default function Home() {
-  return <div className="max-w-7xl mx-auto"></div>;
+  return (
+    <div className="max-w-7xl mx-auto">
+      <Banner />
+    </div>
+  );
 }
