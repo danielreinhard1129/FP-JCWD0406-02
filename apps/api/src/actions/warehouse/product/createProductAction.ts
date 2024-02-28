bac68eb0ca1c8e5b7b961305554ebd187441dc0a
jordy repi <joju19grifith@gmail.com>
2024-02-29 03:39:03 +0700

    fix: fix create product action

--- apps/api/src/actions/warehouse/product/createProductAction.ts
+++ apps/api/src/actions/warehouse/product/createProductAction.ts
@@ -20,7 +20,7 @@ export const createProductAction = async (data: IProduct) => {
       const product = await createProduct(data);
 
       return {
-        message: 'Create Product Success',
+        message: 'Create Product Success!',
         data: product,
       };
     }
