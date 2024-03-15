f4be7ace0c5dacfc06023526c0729b2c43243f2e
Jordyrepi <148455769+Jordyrepi@users.noreply.github.com>
2024-03-15 20:27:09 +0700

    fix: fixing auto mutation (#59)

--- apps/web/src/app/admin/dashboard/warehouse/[warehouse]/orders/page.tsx
+++ apps/web/src/app/admin/dashboard/warehouse/[warehouse]/orders/page.tsx
@@ -1,21 +1,42 @@
 'use client';
 import AdminSidebar from '@/app/admin/components/SidebarDashboard';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { useParams } from 'next/navigation';
 import HeaderWarehouseOrder from './components/HeaderWarehouseOrder';
+import axios from 'axios';
+import { baseUrl } from '@/app/utils/database';
+import CardOrder from './components/CardOrder';
 
 const NotificationSuperAdmin = () => {
   const params = useParams();
+  const [orderList, setOrderList] = useState();
 
-  console.log('ini param', params);
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(
+        `${baseUrl}/transactions/order-list/${params.warehouse}`,
+      );
+      setOrderList(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  console.log('orderrr', orderList?.data);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div className="flex h-screen gap-4 mx-auto max-w-7xl mt-8">
       <AdminSidebar />
       <div className="w-full space-y-10">
         <HeaderWarehouseOrder />
         {/* <StockMutationCard /> */}
-        <div>KONTEN ORDERDISINI UNTUK YANG WAREHOUSE</div>
+        <div>
+          {orderList?.data?.map((data: any) => <CardOrder data={data} />)}
+        </div>
       </div>
     </div>
   );
